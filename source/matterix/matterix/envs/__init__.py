642cb20a805b9c3b66579def3090d1c0148c1065
Arjun Sohal <arjunsohal12@gmail.com>
2025-06-21 16:11:14 -0400

    merge 2

--- source/matterix/matterix/envs/__init__.py
+++ source/matterix/matterix/envs/__init__.py
@@ -7,11 +7,7 @@ import gymnasium as gym
 
 from .matterix_base_env import MatterixBaseEnv
 from .matterix_base_env_cfg import MatterixBaseEnvCfg
-<<<<<<< HEAD
 from . import mdp
-=======
-
->>>>>>> 687cb660b9783b126493db48e9ee7e81006cb459
 ##
 # Register Gym environments.
 ##
