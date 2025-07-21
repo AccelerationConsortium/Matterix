d628eac4ec341e1987d1a0a0d36e85320dd6c895
Arjun Sohal <arjunsohal12@gmail.com>
2025-07-21 17:01:38 -0400

    fix obs

--- source/matterix/matterix/envs/mdp/observations.py
+++ source/matterix/matterix/envs/mdp/observations.py
@@ -21,6 +21,10 @@ from isaaclab.managers.manager_base import ManagerTermBase
 from isaaclab.managers.manager_term_cfg import ObservationTermCfg
 from isaaclab.sensors import Camera, Imu, RayCaster, RayCasterCamera, TiledCamera
 
+from isaaclab.assets import RigidObject
+from isaaclab.managers import SceneEntityCfg
+from isaaclab.utils.math import euler_xyz_from_quat
+from isaaclab.sensors import FrameTransformer
 if TYPE_CHECKING:
     from isaaclab.envs import ManagerBasedEnv, ManagerBasedRLEnv
 
@@ -618,3 +622,20 @@ def remaining_time_s(env: ManagerBasedRLEnv) -> torch.Tensor:
 def force_data(env: ManagerBasedRLEnv, sensor_cfg: SceneEntityCfg = SceneEntityCfg("contact_sensor")):
     
     return env.scene[sensor_cfg.name].data.net_forces_w
+
+
+def ee_position(env: "ManagerBasedEnv", asset_cfg: SceneEntityCfg = SceneEntityCfg("ee_frame")) -> torch.Tensor:
+    ee_frame: FrameTransformer = env.scene[asset_cfg.name]
+    return ee_frame.data.target_pos_w[..., 0, :]
+
+def ee_euler_xyz(env: "ManagerBasedEnv", asset_cfg: SceneEntityCfg = SceneEntityCfg("ee_frame")) -> torch.Tensor:
+    ee_frame: FrameTransformer = env.scene[asset_cfg.name]
+    ee_quat = ee_frame.data.target_quat_w[..., 0, :]
+    roll, pitch, yaw = euler_xyz_from_quat(ee_quat)
+    return torch.stack([roll, pitch, yaw], dim=-1)
+
+
+def object_euler_xyz(env: "ManagerBasedEnv", asset_cfg: SceneEntityCfg) -> torch.Tensor:
+    obj = env.scene[asset_cfg.name]
+    roll, pitch, yaw = euler_xyz_from_quat(obj.data.root_quat_w)
+    return torch.stack([roll, pitch, yaw], dim=-1) 
